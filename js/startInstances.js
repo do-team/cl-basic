d8cb2174c4969a4549f72f00b7f44ef50bebbee1
David Kubec <davesade.42@gmail.co,m>
2017-05-04 14:37:56 +0200

    Added start instances script.

--- js/startInstances.js
+++ js/startInstances.js
@@ -10,7 +10,7 @@ var params = {
     DryRun: false,
     Filters: [{
         Name: 'tag:Service',
-        Values: ['CL-ClouderaCluster']
+        Values: ['testrabbit']
     }]
 };
 
@@ -20,33 +20,29 @@ exports.handler = (event, context, callback) => {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data); // successful response
         var allIds = [];
-        data.Reservations[0].Instances.forEach(function(instance) {
-            allIds = allIds + instance.InstanceId;
-        console.log('I am about to stop this instance for you:' + instance.InstanceId);
+        data.Reservations.forEach(function(reservation){
+            reservation.Instances.forEach(function(instance) {
+                allIds.push(instance.InstanceId);
+                console.log('I am about to start this instance for you:' + instance.InstanceId);
+            });
         });
+
 
         var params = {
             InstanceIds: allIds, // Third parameter of running this script is instance id
-            DryRun: true
+            DryRun: false
         };
 
-/*
-        ec2.stopInstances(params, function(err, data) {
-            if (err && err.code === 'DryRunOperation') {
-                params.DryRun = true;
-                ec2.stopInstances(params, function(err, data) {
+
+        ec2.startInstances(params, function(err, data) {
                     if (err) {
                         console.log("Error", err);
                     } else if (data) {
-                        console.log("Success", data.StoppingInstances);
+                        console.log("Success", data.StartingInstances);
                     }
                 });
-            } else {
-                console.log("You don't have permission to stop instances");
-            }
-        });
 
-*/
+
     });
 
 };
---
+++ js/stopInstances.js
@@ -0,0 +1,55 @@
+var AWS = require('aws-sdk');
+console.log('Loading function');
+
+// Create EC2 service object
+ec2 = new AWS.EC2({
+    apiVersion: '2016-11-15'
+});
+
+var params = {
+    DryRun: false,
+    Filters: [{
+        Name: 'tag:Service',
+        Values: ['testrabbit']
+    }]
+};
+
+exports.handler = (event, context, callback) => {
+
+    ec2.describeInstances(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data); // successful response
+        var allIds = [];
+        data.Reservations.forEach(function(reservation){
+            reservation.Instances.forEach(function(instance) {
+                allIds = allIds + instance.InstanceId;
+                console.log('I am about to stop this instance for you:' + instance.InstanceId);
+            });
+        });
+
+
+        var params = {
+            InstanceIds: allIds, // Third parameter of running this script is instance id
+            DryRun: true
+        };
+
+/*
+        ec2.stopInstances(params, function(err, data) {
+            if (err && err.code === 'DryRunOperation') {
+                params.DryRun = true;
+                ec2.stopInstances(params, function(err, data) {
+                    if (err) {
+                        console.log("Error", err);
+                    } else if (data) {
+                        console.log("Success", data.StoppingInstances);
+                    }
+                });
+            } else {
+                console.log("You don't have permission to stop instances");
+            }
+        });
+
+*/
+    });
+
+};
