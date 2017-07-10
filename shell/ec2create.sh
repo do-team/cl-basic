2aacf793508351e935ea3589846fd5adc58d7d4f
David Kubec <davesade.42@gmail.co,m>
2017-07-10 11:27:35 -0500

    Changed instsance type in shell example

--- shell/ec2create.sh
+++ shell/ec2create.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t1.micro --key-name rm002.pem --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx
+aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name xxxxx.pem --security-group-ids sg-xxxxxxxx --subnet-id subnet-xxxxxxxx
